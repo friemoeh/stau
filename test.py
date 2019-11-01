c3b5478a596b48bcd54f7351b282c7cc83f2ffb8
Frieder Möhler <friemoeh@gmx.de>
2019-11-01 18:51:56 +0100

    first try

--- test.py
+++ test.py
@@ -1,17 +1,36 @@
 from random import randrange
 
+def bremsen(v, delta):
+  if v<delta:
+    return v
+  return v-delta
 
-#time in sec
-t=0
-n=1
+def acc(v, delta):
+  if v<maxV:
+    return v+delta
+  return v
+
+# time in sec
+t = 0
+n = 1
 vall = []
+maxV = 100
 
-for car in range(1, 11):
-    v=randrange(40, 50)
-    vall.append((car,v))
+for car in range(0, n):
+  v = randrange(40, 50)
+  vall.append((car, v))
 
 for t in range(11):
 
-    for car, v in vall :
-        l=t*v
-        print("car %d drove %d meters after %d seconds with the speed %d" %(car, l, t, v) )
+  for car, v in vall:
+    a=randrange(1,3)
+
+    if a == 1:
+        v = bremsen(v, 5)
+    else:
+        v = acc(v, 5)
+        
+    vall[car] = (car,v)
+    l = t*v
+    print("car %d drove %d meters after %d seconds with the speed %d" % (car, l, t, v))
+

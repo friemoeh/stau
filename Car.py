748cd0f1d36871308e8301ab6a01800b3f287086
Frieder Möhler <friemoeh@gmx.de>
2020-04-09 16:56:12 +0200

    react

--- Car.py
+++ Car.py
@@ -18,26 +18,25 @@ class Car:
 
     def update(self, dt):
         self.velocity += self.acceleration * dt
-        self.velocity = max(-self.max_velocity, min(self.velocity, self.max_velocity))
+        self.velocity = max(0, min(self.velocity, self.max_velocity))
 
         self.position += self.velocity * dt
         if self.position > self.lap:
             self.position = self.position - self.lap
 
     def react(self, dt):
-        if self.distance < self.minDist:
-            self.acceleration = - self.default_acc
+        #beschleunige
+        acc = self.default_acc
 
+        #trödeln in probDeacc Fällen
+        reactProb = random()
+        if(reactProb < self.probDeacc):
+            acc = - self.default_acc
+
+        #s: der zu zurück legende Weg        
+        s = self.velocity * dt + acc * dt * dt /2
+        # wenn man das Vorausfahrende FAhrzeug überholen würde dann wird abgebremst
+        if(s>self.distance):
+            self.acceleration = 2 * (self.distance - self.velocity * dt) / (dt * dt)
         else:
-            prob = random()
-
-            if(prob>self.probAcc):
-                self.acceleration = self.default_acc
-            elif(prob>self.probAcc + self.probDeacc):
-                self.acceleration = - self.default_acc
-            else:
-                self.acceleration = 0
-
-
-        
-
+            self.acceleration = acc 
